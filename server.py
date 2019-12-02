0e3b0b974d96c56990b8482604f382526fc7906f
xxlvoyager <34537334+xxlvoyager@users.noreply.github.com>
2019-12-02 11:12:28 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from http.server import HTTPServer,CGIHTTPRequestHandler
 server_address = ("", 5000)
-CGIHTTPRequestHandler.cgi_directories = ["/"]
+CGIHTTPRequestHandler.cgi_directories = ["/cgi"]
 
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 httpd.serve_forever()
