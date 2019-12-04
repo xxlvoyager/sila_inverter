c8f942ea5e9a23c70e0feb1e6d661d851fc5d12d
xxlvoyager <34537334+xxlvoyager@users.noreply.github.com>
2019-12-04 15:29:12 +0300

    add config invertor.ini

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import configparser
+config = configparser.ConfigParser()
+config.read('invertor.ini')
+port=config['DEFAULT']['server_port']
 from http.server import HTTPServer,CGIHTTPRequestHandler
 server_address = ("", 5000)
 CGIHTTPRequestHandler.cgi_directories = ["/cgi"]
